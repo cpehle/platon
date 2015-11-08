36de3e668d258e28da78f9de366b6e0ee68c236b
Christian Pehle <christian.pehle@gmail.com>
2015-11-09 00:16:33 +0100

    Still working on continuous integration

--- .travis-ocaml.sh
+++ .travis-ocaml.sh
@@ -2,6 +2,7 @@ BUILD_CMD=${BUILD_CMD:-make}
 set -uex
 # the base opam repository to use for bootstrapping and catch-all namespace
 BASE_REMOTE=${BASE_REMOTE:-git://github.com/ocaml/opam-repository}
+
 opam init -a ${BASE_REMOTE} --comp=4.02.3
 eval $(opam config env)
 opam install depext
